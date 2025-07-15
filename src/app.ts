40bdcc47701746260ae08642844eca96aafc8f95
Rumi Aktar <ce.rumiakter@gmail.com>
2025-07-16 00:24:58 +0600

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
 import express, { Application, NextFunction, Request, Response } from 'express'
 import { todosRouter } from './app/todos/todos.routes';
 import { url } from 'inspector';
+
 const app: Application = express()
 
 
@@ -14,7 +15,7 @@ app.use("/todos", todosRouter)
 app.use("/users", userRouter)
 
 
-app.get('/', (req: Request, res: Response,  next: NextFunction) => {
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
     console.log({
         url: req.url,
         method: req.method,
@@ -23,8 +24,31 @@ app.get('/', (req: Request, res: Response,  next: NextFunction) => {
     next();
     // res.send('Welcome to todos app')
 
-},(req: Request, res: Response) => {
-    res.send('Welcome to todos app')
+},
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            console.log(something)
+            res.send('Welcome to todos app')
+        } catch (error) {
+            next(error)
+        }
+    })
+
+app.get('/error',
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            console.log(something)
+            res.send('Welcome to error')
+        } catch (error) {
+            next(error)
+        }
+    })
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    if (error) {
+        console.log(error)
+        res.status(400).json({ message: "Something went wrong from global eroor handler", error })
+    }
 })
 
 
